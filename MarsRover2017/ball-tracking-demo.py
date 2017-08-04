95ea2a57ed50e5b24366b224247c7209fbf0b089
Yanqing Wu <y433wu@uwaterloo.ca>
2017-08-04 16:25:52 -0400

    reorgnize

--- MarsRover2017/ball-tracking-demo.py
+++ MarsRover2017/ball-tracking-demo.py
@@ -65,15 +65,15 @@ while True:
 	pts.appendleft(center)
 
 	## loop over the set of tracked points
-	#for i in xrange(1, len(pts)):
-		## if either of the tracked points are None, ignore them
-		#if pts[i - 1] is None or pts[i] is None:
-			#continue
+	for i in xrange(1, len(pts)):
+		# if either of the tracked points are None, ignore them
+		if pts[i - 1] is None or pts[i] is None:
+			continue
 
 		## otherwise, compute the thickness of the line and
 		## draw the connecting lines
-		#thickness = int(np.sqrt(args["buffer"] / float(i + 1)) * 2.5)
-		#cv2.line(frame, pts[i - 1], pts[i], (0, 0, 255), thickness)
+		thickness = int(np.sqrt(args["buffer"] / float(i + 1)) * 2.5)
+		cv2.line(frame, pts[i - 1], pts[i], (0, 0, 255), thickness)
 
 	# show the frame to our screen
 	cv2.imshow("Result", frame)
